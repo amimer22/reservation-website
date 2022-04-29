9fe99e99a40dfda27b0b58949aef8775f13c0adc
amimer22 <devealboy@gmail.com>
2022-04-29 03:21:39 +0200

    setting up reservation table

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `specialcode` varchar(250) NOT NULL,
+  `departement` varchar(250) NOT NULL,
+  `doctor` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `phone` varchar(250) NOT NULL, 
+  `name` varchar(250) NOT NULL,
+  `age` int NOT NULL,
+  `date` date NOT NULL,
+  `horaire` time NOT NULL,
+  `comments` varchar(250) NOT NULL,
+  
+  PRIMARY KEY (`id`),
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
