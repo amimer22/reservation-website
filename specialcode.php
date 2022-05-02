c259dc4afee62f26feab146cf08154c496ec0462
amimer22 <devealboy@gmail.com>
2022-05-02 03:49:38 +0200

    checkRandomString inside db to generate another if match

--- specialcode.php
+++ specialcode.php
@@ -2,29 +2,32 @@
 
 
 function generateRandomString($length = 3) {
-    include("main.php");
-    $characters = '0123456789';
+    
+    $characters = '01';
     $charactersLength = strlen($characters);
     $randomString = 'E-MED-';
     for ($i = 0; $i < $length; $i++) {
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
-    $fixedstring = 'E-MED-001';
-    $sql="SELECT * FROM reservation WHERE specialcode='$randomString'";
-    $result=mysqli_query($db_connect,$sql) or die ("La requête a échoué");
-    //echo($result1);
-    
-    $resultcheck=mysqli_num_rows($result);
-    if ($resultcheck>0) {
-        while ($row=mysqli_fetch_assoc($result)) {                     
-        echo $row['specialcode'].'<br>';
-        }
-    } 
-    else {
-        echo 'not found <br>';
-    }
-
-    return $randomString;
-   
+    checkRandomString($randomString);
+    //echo $randomString;  
 }
+
+function checkRandomString($randomString){
+  include("main.php");
+  $sql="SELECT * FROM reservation WHERE specialcode='$randomString'";
+  $result=mysqli_query($db_connect,$sql) or die ("La requête a échoué");
+  
+  $resultcheck=mysqli_num_rows($result);
+  if ($resultcheck>0) {
+      while ($row=mysqli_fetch_assoc($result)) {                     
+          //echo $row['specialcode'].'<br>';
+          generateRandomString();
+      }
+  } 
+  else {
+      echo $randomString;
+  } 
+}
+
 echo generateRandomString();
