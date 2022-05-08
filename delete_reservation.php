d83c8577a1a1b7c16b664ef2193822c35924e65c
amimer22 <devealboy@gmail.com>
2022-05-08 06:41:44 +0200

    sql for delete

--- delete_reservation.php
+++ delete_reservation.php
@@ -3,4 +3,11 @@ include("main.php");
 $specialCodeDelete=$_REQUEST['specialcode'];
 $nomDelete=$_REQUEST['name'];
 $emailDelete=$_REQUEST['email'];
+
+//check if exists
+$sql = "DELETE FROM reservation WHERE specialcode='$specialCodeDelete' AND name='$nomDelete' AND email ='$emailDelete' ";
+//delete if exists
+
+
+//
 ?>
