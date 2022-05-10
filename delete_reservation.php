a76b4d1253d127760406e59af007b54f42ac34f0
amimer22 <devealboy@gmail.com>
2022-05-10 20:30:36 +0200

    delete reservation after check

--- delete_reservation.php
+++ delete_reservation.php
@@ -9,22 +9,24 @@ $emailDelete=$_REQUEST['email'];
 $sqlcheck = "SELECT * FROM reservation WHERE specialcode='$specialCodeDelete' AND name='$nomDelete' AND email ='$emailDelete' ";
 $checkSqlResult=mysqli_query($db_connect,$sqlcheck) or die ("La requête a échoué check");
 //
+
 $checkResult=mysqli_num_rows($checkSqlResult);
 //
   if ($checkResult>0) {
       while ($row=mysqli_fetch_assoc($checkSqlResult)) {                     
         //check if exists
-        //$sql = "DELETE * FROM reservation WHERE specialcode='$specialCodeDelete' AND name='$nomDelete' AND email ='$emailDelete' ";
+        $sql = "DELETE  FROM reservation WHERE specialcode='$specialCodeDelete'";
         //delete if exists
-        //$delete=mysqli_query($db_connect,$sql) or die ("La requête a échoué anuller");
-        //header("location:apres_annulation.html");
-        echo "not ok"; 
+        $delete=mysqli_query($db_connect,$sql) or die ("La requête a échoué anuller");
+        header("location:apres_annulation.html");
+        //echo "not ok"; 
       }
   } 
   else {
-        echo "ok";
+       echo "Données inserées existent pas";
+
   } 
   
-  mysqli_close($db_connect);
+
 
 ?>
