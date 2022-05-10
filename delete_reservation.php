b3423a3a5a5660a173ff2928e360a94f9f355506
amimer22 <devealboy@gmail.com>
2022-05-10 19:51:59 +0200

    checking if data form inserted in db

--- delete_reservation.php
+++ delete_reservation.php
@@ -1,15 +1,30 @@
 <?php
 include("main.php");
+//
 $specialCodeDelete=$_REQUEST['specialcode'];
 $nomDelete=$_REQUEST['name'];
 $emailDelete=$_REQUEST['email'];
 
 //check if exists
-$sql = "DELETE FROM reservation WHERE specialcode='$specialCodeDelete' AND name='$nomDelete' AND email ='$emailDelete' ";
-//delete if exists
-$delete=mysqli_query($db_connect,$sql) or die ("La requête a échoué");
-
-mysqli_close($db_connect);
-header("location:apres_annulation.html");
+$sqlcheck = "SELECT * FROM reservation WHERE specialcode='$specialCodeDelete' AND name='$nomDelete' AND email ='$emailDelete' ";
+$checkSqlResult=mysqli_query($db_connect,$sqlcheck) or die ("La requête a échoué check");
 //
+$checkResult=mysqli_num_rows($checkSqlResult);
+//
+  if ($checkResult>0) {
+      while ($row=mysqli_fetch_assoc($checkSqlResult)) {                     
+        //check if exists
+        //$sql = "DELETE * FROM reservation WHERE specialcode='$specialCodeDelete' AND name='$nomDelete' AND email ='$emailDelete' ";
+        //delete if exists
+        //$delete=mysqli_query($db_connect,$sql) or die ("La requête a échoué anuller");
+        //header("location:apres_annulation.html");
+        echo "not ok"; 
+      }
+  } 
+  else {
+        echo "ok";
+  } 
+  
+  mysqli_close($db_connect);
+
 ?>
