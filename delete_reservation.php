8432775a8903e61078144a41b592e9f68a273de0
amimer22 <devealboy@gmail.com>
2022-05-11 21:42:23 +0200

    handling form with jquery+ajax to call back success or err msg

--- delete_reservation.php
+++ delete_reservation.php
@@ -1,9 +1,10 @@
 <?php
+sleep(2);
 include("main.php");
 //
-$specialCodeDelete=$_REQUEST['specialcode'];
-$nomDelete=$_REQUEST['name'];
-$emailDelete=$_REQUEST['email'];
+$specialCodeDelete=$_REQUEST['specialcode1'];
+$nomDelete=$_REQUEST['name1'];
+$emailDelete=$_REQUEST['email1'];
 
 //check if exists
 $sqlcheck = "SELECT * FROM reservation WHERE specialcode='$specialCodeDelete' AND name='$nomDelete' AND email ='$emailDelete' ";
@@ -18,14 +19,15 @@ $checkResult=mysqli_num_rows($checkSqlResult);
         $sql = "DELETE  FROM reservation WHERE specialcode='$specialCodeDelete'";
         //delete if exists
         $delete=mysqli_query($db_connect,$sql) or die ("La requête a échoué anuller");
-        header("location:apres_annulation.html");
-        //echo "not ok"; 
+        //header("location:apres_annulation.html");
+        echo "<p> ok </p>"; 
       }
   } 
   else {
        echo "Données inserées existent pas";
 
-  } 
+    }
+  
   
 
 
