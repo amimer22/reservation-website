d777bcf147e498bf635328958aa5b1d0fa863f32
amimer22 <devealboy@gmail.com>
2022-05-20 01:07:43 +0200

    centring map

--- apres_reservation.php
+++ apres_reservation.php
@@ -144,7 +144,7 @@
     <hr>
     <footer class="">
         <div class="container">
-          <div class="map">
+          <div class="map centring">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25583.532688415577!2d3.06069184003464!3d36.72396411574034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128fad41c48b28b5%3A0x85ce2eadf67d55fe!2sKouba!5e0!3m2!1sfr!2sdz!4v1653001093491!5m2!1sfr!2sdz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
           <div class="network">
